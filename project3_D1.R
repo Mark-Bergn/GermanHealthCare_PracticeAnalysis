844f9de98e04e3b7d423d52ebb5f9ba29e6f80bc
Mark Bergenholtz <markbergen0@gmail.com>
2016-11-27 21:35:22 -0800

    Update project3_D1.R
    quantitative variables split between continuous and discrete types. Code also added for recursive cullen and frey plots

--- project3_D1.R
+++ project3_D1.R
@@ -1,4 +1,4 @@
-#R Project 3 Fuck Fuck Around: Round 1;
+#R Project 3 Github Copy
 library(car)
 library(lmtest)
 library(sandwich)
@@ -26,11 +26,11 @@ dfile.na<-read.csv("Assignments/Project 3/german_healthcare_usage.csv", header=T
     names(dfile)
     length(names(dfile))
     str(dfile) 
-    names()
     
     
     
-#Histogram and Barplots
+    
+#Subsetting and Sorting Data
   qual<-cbind(dfile$FEMALE,dfile$HANDDUM, dfile$TRAVEL, dfile$HHKIDS, dfile[,11:21], dfile[,24:26], dfile$DOCTOR,dfile$HEALTHY)
   head(dfile[,11])
   head(dfile[,"MARRIED"])
@@ -39,23 +39,51 @@ dfile.na<-read.csv("Assignments/Project 3/german_healthcare_usage.csv", header=T
   quant=dfile[,-c(1:2,4,6,8,10:20,23:25,28:36,40)]
   colnames(qual)<-colnames(dfile[c(1:2,4,6,8,10:20,23:25,28:36,40)])
   colnames(quant)<-colnames(dfile[-c(1:2,4,6,8,10:20,23:25,28:36,40)])
+  summary(quant)
+  quant<-quant[,c(1:7,9:14)]#Removing HSAT b/c NEWSAT is better
+  summary(quant)
   head(qual)
   head(quant)
   ncol(qual)
   ncol(quant)
-  barplot(quant[,2])
-  as.factor(qual[,1])
-#Histogram for Quantitive Variables:
-for (i in 1:14){
-  truehist(quant[,i],col="skyblue3",ylab="Counts",xlab="Values",main=colnames(quant[,i]))
+ 
+  
+  
+
+#Sorting quant data by countinous vs. discrete:
+summary(quant)
+unique(quant$EDUC) #Not sure how to classify
+quant.d<-cbind(quant$AGE, quant$DOCVIS, quant$HOSPVIS, quant$NUMOBS, quant$TI, quant$NEWHSAT, quant$PRESCRIP)
+quant.c<-cbind(quant$ALC, quant$HANDPER, quant$EDUC, quant$INCOME, quant$LOGINC, quant$HHNINC)
+
+
+
+#Cullen and Frey for Discrete Quantitative Variables;
+dim(quant.d)
+  for (i in 1:7){
+   descdist(quant.d[,i], discrete = TRUE, boot=1000)
+  }
+  
+#Cullen and Frey for Continuous Quantitative Variables;
+dim(quant.c)
+for (i in 1:6){
+  descdist(quant.c[,i], discrete = TRUE, boot=1000)
 }
+
+#Histogram for Quantitive Variables:
+
+for (i in 1:14){
+  truehist(quant[,i],col="skyblue3",ylab="Counts",xlab="Values")
+}
+  
+
 #main function not working for title:
 
 #barplots for qualitative Variables  
 for (i in 1:20){
-  barplot(qual[,i],col="skyblue3",ylab="Counts",xlab="Values",main=colnames(qual[,i]))
+   barplot(qual[,i],col="skyblue3",ylab="Counts",xlab="Values",main=colnames(qual[,i]))
 }
-names(dfile)
-summary(dfile[,"NEWHSAT"])
-dfile[,"NEWHSAT"]
-summary(dfile)[]
+
+#^Figure out better plots display
+  
+
